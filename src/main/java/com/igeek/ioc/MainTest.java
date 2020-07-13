5c2d367182a68c5052b51521823ebafa642cccca
chenziqing321 <2869312537@qq.com>
2020-07-13 18:56:06 +0800

    spring学习

---
+++ src/main/java/com/igeek/ioc/MainTest.java
@@ -0,0 +1,17 @@
+package com.igeek.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        //1.创建IOC容器对象
+        ApplicationContext ac = new ClassPathXmlApplicationContext("beans_ioc.xml");
+        //2.注入实例bean  通过getBean(Class class)要求当前IOC容器中有且只能有一个实例bean
+        Person person = ac.getBean(Person.class);
+        System.out.println(person);
+    }
+
+}
